6b1180fa75e4c97cb4fc23ef251fcf6a6b421c5b
manon gardin <manon.gardin@edu.devinci.fr>
2024-12-11 14:42:32 +0100

    Fix undefined vars

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({path: "/backend"});
 
-const { DB_HOST, DB_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
+const DB_HOST = process.env.DB_HOST as string
+const DB_PORT = process.env.DB_PORT as string
+const POSTGRES_DB = process.env.POSTGRES_DB as string
+const POSTGRES_USER = process.env.POSTGRES_USER as string
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD as string
 
 const sequelize = new Sequelize(
     POSTGRES_DB,
