6b133baacdcbd37b618df63d156ed5c925f86d89
manon gardin <manon.gardin@edu.devinci.fr>
2024-12-19 16:48:25 +0100

    Add routes for production planifiee et controle qualite

---
+++ backend/routes/productionplanifiee.routes.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from "express";
+import { ProductionPlanifiee } from "../models/productionplanifiee.model";
+
+const router = express.Router();
+
+// Obtenir toutes les productions planifiée
+router.get("/", async (req: Request, res: Response) => {
+    try {
+        const productionPlanifiees = await ProductionPlanifiee.findAll();
+        res.json(productionPlanifiees);
+    } catch (err) {
+        res.status(500).json({ error: "Unable to fetch productionPlanifiees" });
+    }
+});
+
+// Ajouter une production planifiée
+router.post("/", async (req: Request, res: Response) => {
+    try {
+        const productionPlanifiee = await ProductionPlanifiee.create(req.body);
+        res.status(201).json(productionPlanifiee);
+    } catch (err) {
+        res.status(500).json({ error: "Unable to create productionPlanifiee" });
+    }
+});
+
+// Mettre à jour une production planifiée
+router.put("/:id", async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const [updated] = await ProductionPlanifiee.update(req.body, { where: { id } });
+
+        if (updated) {
+            const updatedProd = await ProductionPlanifiee.findByPk(id);
+            res.json(updatedProd);
+        } else {
+            res.status(404).json({ error: "ProductionPlanifiee not found" });
+        }
+    } catch (err) {
+        res.status(500).json({ error: "Unable to update ProductionPlanifiee" });
+    }
+});
+
+// Supprimer une production planifiée
+router.delete("/:id", async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const deleted = await ProductionPlanifiee.destroy({ where: { id } });
+
+        if (deleted) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ error: "Production planifiee not found" });
+        }
+    } catch (err) {
+        res.status(500).json({ error: "Unable to delete production planifiee" });
+    }
+});
+
+export default router;
