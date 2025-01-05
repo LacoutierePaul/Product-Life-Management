08a4832c6cc7791c9c6ed4cc819a9cc0f25fe649
manon gardin <manon.gardin@edu.devinci.fr>
2025-01-05 16:04:42 +0100

    Define inbetween table relations

--- backend/models/relations.model.ts
+++ backend/models/relations.model.ts
@@ -7,48 +7,53 @@ import { MouvementStock } from "./mouvementstocks.model";
 import { Fournisseur } from "./fournisseur.model";
 import { FournisseursToStocks } from "./fournisseurstostocks.model";
 
-// Définition des relations
-ProductionPlanifiee.belongsTo(Recette, {
-    foreignKey: "idrecette",
-    onDelete: "CASCADE",
-});
-Recette.hasMany(ProductionPlanifiee, {
-    foreignKey: "idrecette",
-});
+export default function defineRelations() {
 
-ControleQualite.belongsTo(ProductionPlanifiee, {
-    foreignKey: "idproductionplanifiee",
-    onDelete: "CASCADE",
-});
-ProductionPlanifiee.hasMany(ControleQualite, {
-    foreignKey: "idproductionplanifiee",
-});
+// Définition des relations
+    ProductionPlanifiee.belongsTo(Recette, {
+        foreignKey: "idrecette",
+        onDelete: "CASCADE",
+    });
+    Recette.hasMany(ProductionPlanifiee, {
+        foreignKey: "idrecette",
+    });
+
+    ControleQualite.belongsTo(ProductionPlanifiee, {
+        foreignKey: "idproductionplanifiee",
+        onDelete: "CASCADE",
+    });
+    ProductionPlanifiee.hasMany(ControleQualite, {
+        foreignKey: "idproductionplanifiee",
+    });
 
 // **2. RecetteToStocks**
-Recette.belongsToMany(Stock, {
-    through: RecetteToStocks,
-    foreignKey: "idrecette",
-});
-Stock.belongsToMany(Recette, {
-    through: RecetteToStocks,
-    foreignKey: "idstock",
-});
+    Recette.belongsToMany(Stock, {
+        through: RecetteToStocks,
+        foreignKey: "idrecette",
+        otherKey: "idstock"
+    });
+    Stock.belongsToMany(Recette, {
+        through: RecetteToStocks,
+        foreignKey: "idstock",
+        otherKey: "idrecette"
+    });
 
 // **3. MouvementStocks**
-MouvementStock.belongsTo(Stock, {
-    foreignKey: "idstock",
-    onDelete: "CASCADE",
-});
-Stock.hasMany(MouvementStock, {
-    foreignKey: "idstock",
-});
+    MouvementStock.belongsTo(Stock, {
+        foreignKey: "idstock",
+        onDelete: "CASCADE",
+    });
+    Stock.hasMany(MouvementStock, {
+        foreignKey: "idstock",
+    });
 
 // **4. FournisseurToStocks**
-Fournisseur.belongsToMany(Stock, {
-    through: FournisseursToStocks,
-    foreignKey: "idfournisseur",
-});
-Stock.belongsToMany(Fournisseur, {
-    through: FournisseursToStocks,
-    foreignKey: "idstock",
-});
+    Fournisseur.belongsToMany(Stock, {
+        through: FournisseursToStocks,
+        foreignKey: "idfournisseur",
+    });
+    Stock.belongsToMany(Fournisseur, {
+        through: FournisseursToStocks,
+        foreignKey: "idstock",
+    });
+}
