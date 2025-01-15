2b03d6c565c237c3625e66690d93379cc645d0f5
Mattcodman <matias.ottensen@edu.devinci.fr>
2025-01-15 15:32:13 +0100

    Login Page done

---
+++ frontend/src/pages/LoginPage/loginPage.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './loginPage.css';
+
+const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Email:', email);
+        console.log('Password:', password);
+        // Add your authentication logic here
+    };
+
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Email:</label>
+                    <input 
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Password:</label>
+                    <input 
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button className= "buttonlogin" type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginPage;
